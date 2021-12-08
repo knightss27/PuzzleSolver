34e1712e3a81c114d87b9504063a0825253ce622
Seth Knights <knightss@catlin.edu>
2021-12-07 23:24:42 -0800

    New AB stuff.

--- src/players/AlphaBetaPlayer.java
+++ src/players/AlphaBetaPlayer.java
@@ -1,10 +1,14 @@
 package players;
 
+import games.connect4.Connect4State;
 import main.*;
 
 public class AlphaBetaPlayer extends Player {
     final Evaluator evaluator;
     final int maxDepth;
+
+    int alpha;
+    int beta;
 
     public AlphaBetaPlayer(int maxDepth, Evaluator evaluator) {
         this.evaluator = evaluator;
@@ -12,7 +16,51 @@ public class AlphaBetaPlayer extends Player {
     }
 
     public SearchNode pickMove(State currentState) {
-        return null;
+        Connect4State connect4State = (Connect4State) currentState;
+
+        boolean isMaxi = connect4State.getSideToPlay() == Side.ONE;
+        int optimized = isMaxi ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        SearchNode bestNode = connect4State.listChildren().get(0);
+
+        alpha = Integer.MIN_VALUE;
+        beta = Integer.MAX_VALUE;
+
+        for (SearchNode child : connect4State.listChildren()) {
+            int evaluated = childEvaluation(child.state, 0, !isMaxi);
+            if ((!isMaxi && evaluated < optimized) || (isMaxi && evaluated > optimized)) {
+                optimized = evaluated;
+                bestNode = child;
+            }
+        }
+        return bestNode;
+    }
+
+    private int childEvaluation(State state, int depth, boolean maximize) {
+        if (depth >= maxDepth || state.isGameOver()) {
+            return evaluator.evaluate(state);
+        } else {
+            int optimized = maximize ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (SearchNode child : state.listChildren()) {
+                int evaluated = childEvaluation(child.state, depth+1, !maximize);
+                if ((maximize && evaluated > optimized) || (!maximize && evaluated < optimized)) {
+                    optimized = evaluated;
+                }
+
+                if (maximize && optimized > alpha) {
+                    alpha = optimized;
+                } else if (!maximize && optimized < beta) {
+                    beta = optimized;
+                }
+
+                if (maximize && optimized >= beta) {
+                    break;
+                } else if (!maximize && optimized <= alpha) {
+                    break;
+                }
+            }
+
+            return optimized;
+        }
     }
 
     public String toString() {
--- src/players/MiniMaxPlayer.java
+++ src/players/MiniMaxPlayer.java
@@ -1,5 +1,6 @@
 package players;
 
+import games.connect4.Connect4State;
 import main.*;
 
 import java.util.List;
@@ -14,7 +15,35 @@ public class MiniMaxPlayer extends Player {
     }
 
     public SearchNode pickMove(State currentState) {
-       return null;
+        Connect4State connect4State = (Connect4State) currentState;
+
+        boolean isMaxi = connect4State.getSideToPlay() == Side.ONE;
+        int optimized = isMaxi ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        SearchNode bestNode = connect4State.listChildren().get(0);
+
+        for (SearchNode child : connect4State.listChildren()) {
+            int evaluated = childEvaluation(child.state, 0, !isMaxi);
+            if ((!isMaxi && evaluated < optimized) || (isMaxi && evaluated > optimized)) {
+                optimized = evaluated;
+                bestNode = child;
+            }
+        }
+        return bestNode;
+    }
+
+    private int childEvaluation(State state, int depth, boolean maximize) {
+        if (depth >= maxDepth || state.isGameOver()) {
+            return evaluator.evaluate(state);
+        } else {
+            int optimized = maximize ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (SearchNode child : state.listChildren()) {
+                int evaluated = childEvaluation(child.state, depth+1, !maximize);
+                if ((maximize && evaluated > optimized) || (!maximize && evaluated < optimized)) {
+                    optimized = evaluated;
+                }
+            }
+            return optimized;
+        }
     }
 
     public String toString() {
